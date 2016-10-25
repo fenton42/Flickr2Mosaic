a9cc7222bd0d5b1c8172cdcb98d3f9af4ea1ecc1
Tobias Overkamp <fenton42@tov.io>
2016-10-25 19:18:28 +0200

    starting with a CLI class

--- lib/Flickr2Mosaic.rb
+++ lib/Flickr2Mosaic.rb
@@ -1,5 +1,7 @@
 require "Flickr2Mosaic/version"
 
+require 'Flickr2Mosaic/c_l_i'
+
 module Flickr2Mosaic
   # Your code goes here...
 end
--- spec/Flickr2Mosaic_spec.rb
+++ spec/Flickr2Mosaic_spec.rb
@@ -5,7 +5,11 @@ describe Flickr2Mosaic do
     expect(Flickr2Mosaic::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "provides a command line tool that is executable" do
+    expect(File.stat("./exe/flickr2mosaic").executable?).to be_truthy
+  end
+
+  it "should be able to call the CLI via it's class" do
+    expect{CLI.start}.to_not raise_error
   end
 end
