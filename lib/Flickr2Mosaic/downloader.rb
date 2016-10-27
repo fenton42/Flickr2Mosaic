d9735ed2e60ead4f60652b252be199f4eb5e4768
Tobias Overkamp <fenton42@tov.io>
2016-10-27 23:29:24 +0200

    downloader downloads

--- lib/Flickr2Mosaic/downloader.rb
+++ lib/Flickr2Mosaic/downloader.rb
@@ -6,15 +6,21 @@ module Flickr2Mosaic
       self.output_dir = params[:output_dir] || '/tmp/'
     end
 
+    #Just downloads the file from the given url
+    #it will raise an exception if something goes wrong.
+    #I don't see the point in catching it to just make a nicer text...
+    #for example: if the filename can't be written
+    #Errno::ENOENT: No such file or directory @ rb_sysopen - a/tmp/29969001523_f45c759c80_b.jpg
+    #
+    #returns the full path of the image
     def download(url,filename=nil)
-      unless Dir.exists? self.output_dir
-        raise "The desired output directory #{self.output} does not exist"
-      end
+      path = File.join(self.output_dir,filename_from_url(url))
       open(url) do |f|
-        File.open(File.join(self.output_dir,filename_from_url(url)),"wb") do |file|
+        File.open(path,"wb") do |file|
           file.puts f.read
         end
       end 
+      path
     end
 
     private 
